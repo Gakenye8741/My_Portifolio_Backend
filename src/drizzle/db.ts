abbb5513b5616816ca3923c8749d281946c16d13
Gakenye8741 <brayogakenye04@gmail.com>
2025-09-15 13:03:22 +0300

    Adde Online Features to do with drizzle

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,18 +1,19 @@
-import "dotenv/config"
-import { Client } from "pg"
-import {drizzle} from "drizzle-orm/node-postgres";
-import * as schema from "./schema"
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import * as schema from "./schema";
 
-
-export const client = new Client({
-    connectionString: process.env.DATABASE_URL as string
-});
-
-const main = async () =>{
-    await client.connect(); //connect to the database   
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined in environment variables");
 }
 
-main().catch(console.error)
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // if needed by Neon, often required
+  },
+});
 
-const db = drizzle(client,{schema, logger:true});
+const db = drizzle(pool, { schema, logger: true });
+
 export default db;
